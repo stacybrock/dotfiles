c8c3d55827d67539104420c7764851978ae89175
Stacy Brock <brock@oregonstate.edu>
2022-01-24 08:39:14 -0800

    Update kick_mailfilter.sh

--- bin/kick_mailfilter.sh
+++ bin/kick_mailfilter.sh
@@ -1,32 +1,49 @@
 #!/usr/bin/env bash
 
-MAILFILTER_DIR=$HOME/tools/mail-filter
+MAILFILTER_DIR=$HOME/tools/o365-mail-filter
 
 KILL_ONLY=false
-while getopts ":k" opt; do
+PASS_ONLY=false
+while getopts ":kp" opt; do
   case ${opt} in
-    k ) # process option h
+    k ) # process option k
         KILL_ONLY=true
       ;;
+    p ) # process option p
+        PASS_ONLY=true
+      ;;
     \? )
-        echo "Usage: kill_mailfilter.sh [-k]"
+        cat << EOF
+Usage: kick_mailfilter.sh [-k] [-p]
+
+If no options are given, runs the mailfilter script
+
+Options
+  -k  terminate running mailfilter
+  -p  read creds into envvars out of pass
+EOF
         exit 1
       ;;
   esac
 done
 shift $((OPTIND -1))
 
-PID=`pgrep -f mail-filter.py`
-
-# kill mail-filter.py if it's running
-if [[ $PID != "" ]]; then
-    echo "killed pid: $PID"
-
-    kill -9 $PID
-fi
-
 # check for -k (kill only) flag and bail early if it's set
 if $KILL_ONLY; then
+    launchctl stop local.mailfilter
+    echo `launchctl list | grep local.mailfilter`
+    exit 0
+fi
+
+# get username/password from pass, then bail out
+if $PASS_ONLY; then
+    # IFS=':' read -ra creds <<< `pass OSU/ONID`
+    app_client_id=`pass Azure/brock-mail-filter/client_id`
+    app_secret_key=`pass Azure/brock-mail-filter/secret_key`
+    app_tenant_id=`pass Azure/oregonstateuniv_tenant_id`
+    launchctl setenv APP_CLIENT_ID $app_client_id
+    launchctl setenv APP_SECRET_KEY $app_secret_key
+    launchctl setenv APP_TENANT_ID $app_tenant_id
     exit 0
 fi
 
@@ -34,6 +51,3 @@ fi
 source $HOME/.virtualenvs/mail-filter/bin/activate
 cd $MAILFILTER_DIR
 python mail-filter.py
-
-PID=`pgrep -f mail-filter.py`
-echo "new pid: $PID"
